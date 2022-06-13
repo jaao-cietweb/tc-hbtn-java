58cbbb22518cc1138f5c2086915867df2c82540f
joaovg@ciandt.com <joaovg@ciandt.com>
2022-06-13 14:17:31 -0300

    Commit 2_ex1

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -1,6 +1,6 @@
 public class PrintIntegerVariable {
     public static void main (String[] args){
         int numero = 311;
-        System.out.println("Avenida Andaló " + numero);
+        System.out.println("Avenida Andaló, " + numero);
     }
 }
