f2ecad9bd0214bcf2cf65b7e20cdb2b4afcd654e
joaovg@ciandt.com <joaovg@ciandt.com>
2022-06-13 16:21:29 -0300

    Commit 1_ex4

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -2,7 +2,7 @@ public class PrintStringConcat {
     public static void main (String[] args) {
         String texto1 = "Holberton";
         String texto2 = "School";
-        String finall = "Bem vindo a " + texto1 + " "+ texto2;
+        String finall = "Bem vindo a " + texto1 + " " + texto2 + "!";
         System.out.println(finall);
     }
 }
