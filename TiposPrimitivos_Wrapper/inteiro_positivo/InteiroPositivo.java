ee5c7d3550118ce30b0f0cb611107426fcc9d1c9
joaovg@ciandt.com <joaovg@ciandt.com>
2022-06-14 14:25:33 -0300

    Commit Tipos Primitivos e Wrappers - 1

---
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -0,0 +1,41 @@
+public class InteiroPositivo {
+    private int valor;
+
+    public InteiroPositivo (int valor){
+        if(valor <= 0){
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+        }
+        else {
+            this.valor = valor;
+        }
+    }
+
+    public InteiroPositivo (String valor){
+        if(Integer.parseInt(valor) <= 0) {
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+        }
+        else {
+            this.valor = Integer.parseInt(valor);
+        }
+    }
+
+    boolean ehPrimo (){
+        for (int n = 2; n < valor; n++ ){
+            if(valor % n == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        if (valor <= 0){
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+        }
+        this.valor = valor;
+    }
+}
