d025c4a3a332813d9c5c68d02b37a46847326a0a
joaovg@ciandt.com <joaovg@ciandt.com>
2022-06-15 10:14:52 -0300

    Commit Classes e Objetos - ex3

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,50 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public void tomarDano (int quantidadeDeDano){
+        this.saudeAtual = saudeAtual - quantidadeDeDano;
+        if (saudeAtual < 0) {
+            saudeAtual = 0;
+            status = "morto";
+        }
+    }
+
+    public void receberCura (int quantidadeDeCura){
+        this.saudeAtual  = saudeAtual + quantidadeDeCura;
+        if (saudeAtual > 100) {
+            saudeAtual = 100;
+        }
+        if (saudeAtual > 0){
+            status = "vivo";
+        }
+    }
+
+    //gettersAndSetters
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual <= 0){
+            status = "morto";
+        }
+        else {
+            status = "vivo";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
