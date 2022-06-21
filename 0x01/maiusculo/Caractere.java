90d132c04e24102f34d1c2a9eaac03a6cf06c33e
joaovg@ciandt.com <joaovg@ciandt.com>
2022-06-21 12:07:41 -0300

    Commit Estruturas, Decisão e Repetição  - ex2

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,8 @@
+public class Caractere {
+    public static boolean EhMaiusculo(char c){
+        if (Character.isUpperCase(c)){
+            return true;
+        }
+        else return false;
+    }
+}
