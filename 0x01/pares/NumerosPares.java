4eb5395a21e382ba258b129b57c52b717a86a34f
joaovg@ciandt.com <joaovg@ciandt.com>
2022-06-21 10:06:57 -0300

    Commit Estruturas, Decisão e Repetição_1  - ex0

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,11 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        int n;
+        for(n = 0; n < 98; n++){
+            if(n%2 == 0) {
+                System.out.print(n + ", ");
+            }
+        }
+        System.out.println(n);
+    }
+}
