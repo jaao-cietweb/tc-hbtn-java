11ed5e242bb77f3cb76f8b2f90bfd0a13dd4a6ca
joaovg@ciandt.com <joaovg@ciandt.com>
2022-06-21 09:55:25 -0300

    Commit Estruturas, Decisão e Repetição  - ex0

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,8 @@
+public class Numeros0a99 {
+    public static void main(String[] args) {
+        for(int n = 0; n < 100; n++){
+            System.out.print(n + ", ");
+        }
+        System.out.println();
+    }
+}
