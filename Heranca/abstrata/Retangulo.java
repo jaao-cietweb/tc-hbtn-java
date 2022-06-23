99f6f72901a8ac5d98e5b8c5b8b9cf842ff85e00
joaovg@ciandt.com <joaovg@ciandt.com>
2022-06-23 15:06:05 -0300

    Commit Herança - ex5

---
+++ Heranca/abstrata/Retangulo.java
@@ -0,0 +1,36 @@
+public class Retangulo extends FormaGeometrica{
+    protected double largura;
+    protected double altura;
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    @Override
+    public double area(){
+        return (altura * largura);
+    }
+
+    @Override
+    public String toString(){
+        return String.format("[Retangulo] %.2f / %.2f", getLargura(), getLargura());
+    }
+
+    public void setAltura(double altura) {
+        if(altura < 0){
+            throw new IllegalArgumentException ("Altura deve ser maior ou igual a 0");
+        }
+        this.altura = altura;
+    }
+
+    public void setLargura(double largura) {
+        if (largura < 0){
+            throw new IllegalArgumentException ("Largura deve ser maior ou igual a 0");
+        }
+        this.largura = largura;
+    }
+}
