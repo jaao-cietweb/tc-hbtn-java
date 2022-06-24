a7142b2b5cbee94c021eaa67f5a9b7353e858df3
joaovg@ciandt.com <joaovg@ciandt.com>
2022-06-24 11:41:58 -0300

    Commit Exceções (Parte II) - ex1

--- 0x03/livraria/Program.java
+++ 0x03/livraria/Program.java
@@ -1,5 +1,9 @@
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
+        final var locale = new Locale("pt", "BR");
+        Locale.setDefault(locale);
         try {
             Livro livro1 = new Livro(
                     "As barbas do imperador: D. Pedro II",
