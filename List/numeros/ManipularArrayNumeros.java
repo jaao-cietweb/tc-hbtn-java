cd9697387ff497f3829be893c179f921e871a904
joaovg@ciandt.com <joaovg@ciandt.com>
2022-06-28 16:00:35 -0300

    Commit Collections (List) - ex0

--- List/numeros/ManipularArrayNumeros.java
+++ List/numeros/ManipularArrayNumeros.java
@@ -2,30 +2,31 @@ import java.util.List;
 
 public class ManipularArrayNumeros {
 
-    static int buscarPosicaoNumero(List<Integer> lista, int num){
+    static int buscarPosicaoNumero(List<Integer> lista, int num) {
         return lista.indexOf(num);
     }
 
     static void adicionarNumero(List<Integer> lista, int num) {
-        if (buscarPosicaoNumero(lista,num) != -1){
+        if (buscarPosicaoNumero(lista, num) != -1) {
             throw new IllegalArgumentException("Numero jah contido na lista");
         }
         lista.add(num);
     }
 
     static void removerNumero(List<Integer> lista, int num) {
-        int pos = buscarPosicaoNumero(lista,num);
-        if (pos == -1){
+        int pos = buscarPosicaoNumero(lista, num);
+        if (pos == -1) {
             throw new IllegalArgumentException("Numero nao encontrado na lista");
         }
         lista.remove(pos);
     }
 
-    static void substituirNumero (List<Integer> lista, int numSubstituir, int numSubistituido) {
-        int pos = buscarPosicaoNumero(lista,numSubstituir);
-        if (pos == -1){
-            throw new IllegalArgumentException("Numero nao encontrado na lista");
+    static void substituirNumero(List<Integer> lista, int numSubstituir, int numSubistituido) {
+        int pos = buscarPosicaoNumero(lista, numSubstituir);
+        if (pos == -1) {
+            lista.add(numSubistituido);
+        } else {
+            lista.set(pos, numSubistituido);
         }
-        lista.set(pos, numSubistituido);
     }
 }
