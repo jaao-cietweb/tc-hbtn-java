9f5ad1f98790ff26b7f5c1582a6007bf97632f89
joaovg@ciandt.com <joaovg@ciandt.com>
2022-07-01 13:52:48 -0300

    Commit Collections (Set e Map) - ex3

---
+++ Set_Map/telefones_duplicados/ListaTelefonica.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class ListaTelefonica {
+    HashMap <String, HashSet<Telefone>> contatos;
+
+    public ListaTelefonica() {
+        contatos = new HashMap<String, HashSet<Telefone>>();
+    }
+
+    public void adicionarTelefone (String nome, Telefone telefone){
+        HashSet<Telefone> array = contatos.get(nome);
+
+        contatos.forEach((chave, valor) ->{
+            if (chave.equals(nome) && valor.contains(telefone)) {
+                throw new IllegalArgumentException("Telefone jah existente para essa pessoa.");
+            } else if (valor.contains(telefone)) {
+                throw new IllegalArgumentException("Telefone jah pertence a outra pessoa.");
+            }
+        });
+
+        if(array == null){
+            array = new HashSet<Telefone>();
+        }
+        array.add(telefone);
+        contatos.put(nome,array);
+    }
+
+    public HashSet<Telefone> buscar (String nome){
+        return contatos.get(nome);
+    }
+}
