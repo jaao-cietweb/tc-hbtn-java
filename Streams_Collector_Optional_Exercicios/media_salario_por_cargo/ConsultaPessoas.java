8f13fa26e25bb153247b0673d26e29c509b473c8
joaovg@ciandt.com <joaovg@ciandt.com>
2022-07-14 14:32:53 -0300

    Commit Streams, Collectors e Optional - Exercícios de fixação - ex3

---
+++ Streams_Collector_Optional_Exercicios/media_salario_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> listaPessoas) {
+        return listaPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, () -> new TreeMap<>(Comparator.reverseOrder()), Collectors.toCollection(TreeSet::new)));
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> listaPessoas) {
+        return listaPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+    }
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> listPessoas) {
+        return listPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,
+                Collectors.groupingBy(Pessoa::getIdade, Collectors.counting())));
+    }
+
+    public static Map<String, Double> obterMediaSalarioPorCargo(List<Pessoa> listPessoas) {
+        return listPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.averagingDouble(Pessoa::getSalario)));
+    }
+}
+
